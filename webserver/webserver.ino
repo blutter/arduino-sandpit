b8123163a0468a7fe864db040dade3c719e16e0f
Benjamin Tse <benjamin.l.tse@gmail.com>
2019-02-02 11:16:28 +1100

    Connect to WiFi

--- webserver/webserver.ino
+++ webserver/webserver.ino
@@ -6,14 +6,16 @@ WiFiServer server(80);
 void setup() {
   Serial.begin(115200); // For debugging output
 
-  Serial.println("Connecting to %s", SSID);
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
   WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("Connected. IP address: %s", WiFi.localIP());
+  Serial.print("Connected. IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
